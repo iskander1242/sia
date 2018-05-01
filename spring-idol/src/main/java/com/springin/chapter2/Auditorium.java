887bfa072297e67ebe1c9dbd7d4f0eacfc5b7f96
Iskander <iskander1240@gmail.com>
2018-05-01 06:29:59 +0300

    added example default-init-method, destroy method

---
+++ spring-idol/src/main/java/com/springin/chapter2/Auditorium.java
@@ -0,0 +1,15 @@
+package com.springin.chapter2;
+
+/**
+ * Created by Alexander Sokolov
+ * on 5/1/18.
+ */
+public class Auditorium {
+    public  void  turnOnLights(){
+        System.out.println("#Auditoriun.turnOnLights");
+    }
+
+    public  void turnOffLights(){
+        System.out.println("#Auditoriun.turnOffLights");
+    }
+}
